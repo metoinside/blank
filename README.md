ff7d6647f68493d41d19e7a4154a1cf507954904
Metin <aknmetin@gmail.com>
2023-06-27 07:58:21 +0000

    Emptied project

--- README.md
+++
@@ -1,3 +0,0 @@
-# Blank App
-
-This is a default application where it shows the current block number of default Infura provider
--- index.js
+++
@@ -1,4 +0,0 @@
-import { ethers } from "ethers";
-
-const provider = new ethers.InfuraProvider();
-console.log ("Current Block Number: ", await provider.getBlockNumber());
