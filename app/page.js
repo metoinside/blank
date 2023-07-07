471053ebeb1aa6d67c17c98c460e8a57e11ad504
Metin <aknmetin@gmail.com>
2023-07-07 12:28:39 +0000

    DaisyUI enabled

--- app/page.js
+++ app/page.js
@@ -3,5 +3,5 @@ import { ethers } from "ethers"
 const provider = new ethers.InfuraProvider("goerli", "11ba91059a854be0889901bf564477c3");
 
 export default async function Home() {
-  return <h1>Current Blocknumber {await provider.getBlockNumber()}</h1>
+  return <h1 className="text-5xl font-bold">Current Blocknumber: {await provider.getBlockNumber()}</h1>
 }
