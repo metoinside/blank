d91f93e83e0becfc9a09ebcbbe9da1d34736ee01
Metin <aknmetin@gmail.com>
2023-07-08 15:25:57 +0000

    Basic app with components are created

---
+++ app/components/divider.js
@@ -0,0 +1,7 @@
+export default function Divider() {
+return (
+<div className="flex flex-col w-full">
+  <div className="divider"></div> 
+</div>
+)
+}
---
+++ app/components/steps.js
@@ -0,0 +1,12 @@
+export default function Steps() {
+  return (
+    <div className="grid justify-items-center p-10">
+      <ul className="steps">
+        <li className="step step-primary">Register</li>
+        <li className="step step-primary">Choose plan</li>
+        <li className="step">Purchase</li>
+        <li className="step">Receive Product</li>
+      </ul>
+      </div>
+  );
+}
